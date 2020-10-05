6f1f8818e7a10563dea3e03897d2fe47b3fe6787
franco <fcedo13@gmail.com>
2020-10-05 19:00:51 -0300

    prueba formulario2

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -59,18 +59,12 @@
 
                     </v-form>
 
-                    <form name="contact" method="POST" data-netlify="true">
+                    <form name="contact" method="POST" data-netlify="true" netlify>
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
   <p>
     <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
   </p>
   <p>
     <label>Message: <textarea name="message"></textarea></label>
