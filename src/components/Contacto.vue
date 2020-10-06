078fd7a4548f6f0de4d1830d1be0643dee8093cf
franco <fcedo13@gmail.com>
2020-10-05 22:14:51 -0300

    prueba formulario2

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -62,6 +62,19 @@
 
                     </v-form>
 
+                    <form name="simple-form" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name"/></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email"/></label>
+  </p>
+  <p>
+    <label>Message: <input type="textarea" name="message"/></label>
+  </p>
+  <p><button type="submit">Send</button></p>
+</form>
+
                     
 
 
